5c0ebc2df84dfaab40f9a1051af88f68430cce95
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-05-11 12:43:03 +0200

    Type

---
+++ index.ts
@@ -0,0 +1,54 @@
+type Pizza = {
+  name: string;
+  price: number;
+};
+
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+let cashInRegister = 100;
+let nextOrderId = 1;
+
+const orderQueue = [];
+
+function addNewPizza(newPizzaObj: Pizza) {
+  menu.push(newPizzaObj);
+}
+
+function placeOrder(pizzaName: string) {
+  let choosenPizza = menu.find((newPizzaObj) => newPizzaObj.name === pizzaName);
+
+  if (!choosenPizza) {
+    console.error(`$(pizzaName) does not exist in the menu`);
+    return;
+  }
+
+  cashInRegister += choosenPizza.price;
+
+  let newOrder = { id: nextOrderId++, pizza: choosenPizza, status: "ordered" };
+
+  orderQueue.push(newOrder);
+
+  return newOrder;
+}
+
+function completeOrder(orderId: number) {
+  let order = orderQueue.find((order) => order.id === orderId);
+  order.status = "completed";
+  return order;
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ Chicken", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 11 });
+
+placeOrder("Chicken Bacon Ranch");
+completeOrder(1);
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
